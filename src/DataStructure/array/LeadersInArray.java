ea24c2c7af4ec91a947c40fd68c0985ecf172a5b
Rutul Darji <rutul.darji@vuclip.com>
2020-06-25 13:50:45 +0530

    Add Leades in Array Program

---
+++ src/DataStructure/array/LeadersInArray.java
@@ -0,0 +1,34 @@
+package DataStructure.array;
+
+/**
+ *
+ * @Author : Rutul Darji
+ * @Description : Write a program to print all the LEADERS in the array. An element is leader if it is greater than all
+ * the elements to its right side. And the rightmost element is always a leader.
+ * @Link : https://www.geeksforgeeks.org/leaders-in-an-array/
+ * */
+
+public class LeadersInArray {
+
+    public void printLeaders(int arr[], int n) {
+
+        int currMax = arr[n-1];
+        System.out.print( currMax+ " ");
+
+        for (int i=n-2;i>=0;i--) {
+            if (currMax< arr[i]) {
+                currMax = arr[i];
+                System.out.print( currMax+ " ");
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        LeadersInArray leadersInArray = new LeadersInArray();;
+        int arr[] = new int[]{16, 17, 4, 3, 5, 2};
+        int n = arr.length;
+        leadersInArray.printLeaders(arr, n);
+    }
+}
